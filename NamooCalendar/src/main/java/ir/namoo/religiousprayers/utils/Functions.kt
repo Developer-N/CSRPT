055aaee497d1ace4388360689dc761b00e73f39a
Developer-N <ahmadnia.ce@gmail.com>
2021-06-23 16:38:31 +0430

    Minor Bug fix

--- NamooCalendar/src/main/java/ir/namoo/religiousprayers/utils/Functions.kt
+++ NamooCalendar/src/main/java/ir/namoo/religiousprayers/utils/Functions.kt
@@ -21,7 +21,6 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.view.inputmethod.InputMethodManager
-import android.widget.FrameLayout
 import android.widget.RelativeLayout
 import android.widget.TextView
 import android.widget.Toast
@@ -678,10 +677,6 @@ fun createBitmapFromView(v: View): Bitmap {
 }
 
 fun createBitmapFromView2(v: View): Bitmap {
-    v.layoutParams = FrameLayout.LayoutParams(
-        FrameLayout.LayoutParams.MATCH_PARENT,
-        FrameLayout.LayoutParams.WRAP_CONTENT
-    )
     v.measure(
         View.MeasureSpec.makeMeasureSpec(0, View.MeasureSpec.UNSPECIFIED),
         View.MeasureSpec.makeMeasureSpec(0, View.MeasureSpec.UNSPECIFIED)
